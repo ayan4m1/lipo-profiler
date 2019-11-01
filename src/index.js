4845286d3875e0b03bb8ccab0af68f28c661ba33
ayan4m1 <andrew@bulletlogic.com>
2019-11-01 13:00:18 -0400

    add ramp-up and ramp-down to test steps

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import Chance from 'chance';
+import moment from 'moment';
+import { writeFile } from 'fs';
 
 import loggers from './logging';
-import moment from '../node_modules/moment/moment';
 
 const log = loggers('app');
 
 const iterations = 1;
 const puffs = {
   count: 5,
+  rampUp: 1000,
   duration: {
     mean: 3000,
     dev: 100
+  },
+  voltage: 3.87,
+  wattage: {
+    heating: 50,
+    cooling: 35
   }
 };
 const idle = {
@@ -29,14 +36,19 @@ const randomIdle = () => random(idle.duration);
 
 const formatTimestamp = (previousFrame, duration) => {
   const currentFrame = moment.duration(previousFrame, 'ms').add(duration, 'ms');
+  const [hours, minutes, seconds] = [
+    currentFrame.hours(),
+    currentFrame
+      .minutes()
+      .toString()
+      .padStart(2, '0'),
+    currentFrame
+      .seconds()
+      .toString()
+      .padStart(2, '0')
+  ];
 
-  return `${currentFrame.hours()}:${currentFrame
-    .minutes()
-    .toString()
-    .padStart(2, '0')}:${currentFrame
-    .seconds()
-    .toString()
-    .padStart(2, '0')}`;
+  return `${hours}:${minutes}:${seconds}`;
 };
 
 let serialNumber = 0;
@@ -50,6 +62,11 @@ const appendLogEntry = (time, voltage, wattage) => {
 const execute = async () => {
   log.info('Started CSV generation...');
 
+  const amps = {
+    heating: (puffs.wattage.heating / puffs.voltage).toPrecision(2),
+    cooling: (puffs.wattage.cooling / puffs.voltage).toPrecision(2)
+  };
+
   for (let iteration = 1; iteration <= iterations; iteration++) {
     log.info(`Simulation iteration ${iteration}`);
     let iterationMs = 0;
@@ -58,20 +75,39 @@ const execute = async () => {
       log.info(`Simulation puff ${puff}`);
 
       const puffMs = randomPuff();
+      const coolingMs = Math.max(0, puffMs - puffs.rampUp);
       const idleMs = randomIdle();
 
       const prePuff = formatTimestamp(iterationMs, 0);
-      const postPuff = formatTimestamp(iterationMs, puffMs);
-      const frameMs = puffMs + idleMs;
 
-      appendLogEntry(prePuff, 14.8, 50);
-      appendLogEntry(postPuff, 14.8, 0);
+      appendLogEntry(prePuff, puffs.voltage, amps.heating);
+
+      if (coolingMs > 0) {
+        const preCool = formatTimestamp(iterationMs, puffs.rampUp);
+
+        appendLogEntry(preCool, puffs.voltage, amps.cooling);
+      }
+
+      const postPuff = formatTimestamp(iterationMs, puffMs + coolingMs);
+
+      appendLogEntry(postPuff, puffs.voltage, 0);
+
+      const frameMs = puffMs + coolingMs + idleMs;
 
       iterationMs += frameMs;
     }
   }
 
-  log.info(`\nNumber,Time,Voltage,Wattage\n${results.join('\n')}`);
+  writeFile(
+    'output.csv',
+    `Number,Time,Voltage,Current\n${results.join('\n')}`,
+    'utf8',
+    error => {
+      if (error) {
+        log.error(error.message, error);
+      }
+    }
+  );
 };
 
 execute();
